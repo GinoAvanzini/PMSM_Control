3889bef813f456ef7dadd814568f5037696ac07b
Gino Avanzini <gino.avanzini@hotmail.com>
2020-09-01 21:39:25 -0300

    Add external position controller (PID)

--- src/PMSM_AyME.m
+++ src/PMSM_AyME.m
@@ -45,3 +45,11 @@ R_cq = -p_i*L_q;
 R_q = -p_i*L_q;
 R_d = -p_i*L_d;
 R_0 = -p_i*Lls;
+
+%controlador PID
+n=2.5;
+w_pos=800;
+%Ganancias PID
+ba=n*J_eq*w_pos;
+Ksa=n*J_eq*w_pos*w_pos;
+Ksia=J_eq*w_pos*w_pos*w_pos;
